2d8d8badb4a5fa722eb7368d8cdde82cff5726a7
Vikas KM <vikki.km@gmail.com>
2018-12-31 20:08:52 +0530

    Updates

---
+++ precache-manifest.cf9e3674d7d19a1b6ed09632e4d5b925.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7f226c0348e899de4d1",
+    "url": "/test-react/static/css/main.f8e0ad91.chunk.css"
+  },
+  {
+    "revision": "e7f226c0348e899de4d1",
+    "url": "/test-react/static/js/main.e7f226c0.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/test-react/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "056c8d9a730cc8cd4a6f",
+    "url": "/test-react/static/js/runtime~main.056c8d9a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "12e1f700720271e4ca36f08827975a81",
+    "url": "/test-react/index.html"
+  }
+];
